03d25061fccab183947903a74d309eed8dd76dcc
artur199207 <artcomputers92@gmail.com>
2023-11-20 17:37:52 +0400

    js

--- script/script.js
+++ script/script.js
@@ -11,3 +11,21 @@ $(function(){
       $('.nav-toggle').toggleClass('is-active');
     })
   })
+
+  const btn = document.querySelectorAll('.onclick');
+  let prevClickedBtn = null;
+  
+  for (let i = 0; i < btn.length; i++) {
+    btn[i].onclick = function () {
+      if (prevClickedBtn !== null) {
+        prevClickedBtn.style.background = ''; 
+      }
+  
+      this.style.background = 'linear-gradient(180deg, #FFB763 10.52%, #FF7233 78.75%)';
+      prevClickedBtn = this; 
+    };
+  }
+
+
+
+ 
